050759f3c548e2501d82177ce31638d05f6d4937
DESKTOP-NTT999I\User <iamimrancse@gmail.com>
2024-08-12 01:21:06 +0600

    Selection sort

--- src/main/java/org/example/sorting/SelectionSort.java
+++ src/main/java/org/example/sorting/SelectionSort.java
@@ -1,4 +1,28 @@
 package org.example.sorting;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] array = {13, 46, 24, 52, 20, 9};
+        int n = array.length;
+
+        for(int i = 0; i<n; i++){
+            int minimum = i;
+            for (int j = i+1; j<n ; j++){
+                if(array[j] < array[minimum]){
+                    minimum = j;
+                }
+            }
+
+            //swap
+            int temp = array[minimum];
+            array[minimum] = array[i];
+            array[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
 }
